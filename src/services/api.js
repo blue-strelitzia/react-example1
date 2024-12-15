414c1b65acdebed9057a7e41a11d4d5e461f4a3b
blue-strelitzia <kunigogo.dev.public2@gmail.com>
2024-12-15 12:18:29 +0000

    added tanstack query

---
+++ src/services/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080";
+const axiosInstance = axios.create({ baseURL: BASE_URL });
+
+export const getTodosIds = async () => {
+  return (await axiosInstance.get("todos")).data.map((todo) => todo.id);
+};
+
+export const getTodo = async (id) => {
+  return (await axiosInstance.get(`todos/${id}`)).data;
+};
+
+export const createTodo = async (data) => {
+  await axiosInstance.post("todos", data);
+};
+
+export const updateTodo = async (data) => {
+  await axiosInstance.put(`todos/${data.id}`, data);
+};
+
+export const deleteTodo = async (id) => {
+  await axiosInstance.delete(`todos/${id}`);
+};
---
+++ src/services/queries.js
@@ -0,0 +1,29 @@
+import {
+    useQuery,
+    useQueries,
+    keepPreviousData,
+    useInfiniteQuery,
+    useQueryClient,
+  } from "@tanstack/react-query";
+  import {
+    getTodo,
+    getTodosIds,
+  } from "./api";
+  
+  export function useTodosIds() {
+    return useQuery({
+      queryKey: ["todos"],
+      queryFn: getTodosIds,
+    });
+  }
+  
+  export function useTodos(ids) {
+    return useQueries({
+      queries: (ids ?? []).map((id) => {
+        return {
+          queryKey: ["todo", { id }],
+          queryFn: () => getTodo(id),
+        };
+      }),
+    });
+  }
